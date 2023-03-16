72d1048507d46a5b9db399675a110bd00b1b2813
Miguel Martinez <miguel.emezero@gmail.com>
2023-03-15 21:31:06 -0600

    👾 feat: types of each commit

---
+++ src/commit-types.js
@@ -0,0 +1,38 @@
+export const COMMIT_TYPES = {
+  feat: {
+    emoji: '👾',
+    description: 'Add new feature',
+    release: true
+  },
+  fix: {
+    emoji: '🐛',
+    description: 'Fix a bug',
+    release: true
+  },
+  perf: {
+    emoji: '⚡',
+    description: 'Improve performance',
+    release: true
+  },
+  refactor: {
+    emoji: '⚒️',
+    description: 'Refactor code',
+    release: true
+  },
+  docs: {
+    emoji: '📚',
+    description: 'Add or update documentation',
+    release: false
+  },
+  test: {
+    emoji: '🧪',
+    description: 'Add or update tests',
+    release: false
+  },
+  build: {
+    emoji: '🏗️',
+    description: 'Add or update build scripts',
+    release: false
+  }
+
+}
